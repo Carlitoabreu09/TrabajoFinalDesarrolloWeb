dbc2f7bfc0851aefcfc4ded1b7e0c967d7db3ba8
Jhon-A-31alphabet <jhonmarcosaraujo@yahoo.com>
2025-12-29 10:11:18 -0400

    todas las rutas agregadas al servidor

--- server.js
+++ server.js
@@ -7,9 +7,25 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
+
+
 app.get("/", (req, res) => {
     res.sendFile(join(__dirname, 'principal.html')); 
 })
+
+app.get("/index",(req,res)=>{
+    res.sendFile(join(__dirname, 'index.html'))
+})
+
+app.get("/clientes",(req,res)=>{
+    res.sendFile(join(__dirname,'clientes.html'))
+})
+
+app.get("/crearfactura",(req,res)=>{
+    res.sendFile(join(__dirname,'crearfactura.html'))
+
+})
+
 
 app.get("/addclientes", (req, res) => {
     res.sendFile(join(__dirname, 'addclientes.html')); 
@@ -26,7 +42,32 @@ app.get("/listadoproductos",(req,res)=>{
 })
 
 
+app.get("/listadoclientes",(req,res)=>{
+        res.sendFile(join(__dirname, 'listadoclientes.html')); 
+
+})
+
+app.get("/listadoproductos",(req,res)=>{
+    res.sendFile(join(__dirname, 'listadoproductos.html'));
+})
+
+app.get("/addproductos",(req,res)=>{
+    res.sendFile((join(__dirname, 'addproductos.html')));
+})
+
+
+app.get("/productos",(req,res)=>{
+    res.sendFile((join(__dirname, 'productos.html')));
+})
+
+app.get("/facturacion",(req,res)=>{
+    res.sendFile((join(__dirname, 'facturacion.html')));
+})
+
+
+
 app.set("port", process.env.PORT || 3000);
+
 
 // Run server
 app.listen(app.get("port"), () => {
