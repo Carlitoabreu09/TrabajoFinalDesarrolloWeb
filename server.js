bc3717e5a6c5e5d1ded72c04b660fb468524f9dd
Jhon-A-31alphabet <jhonmarcosaraujo@yahoo.com>
2026-01-08 21:31:57 -0400

    leve modificacion del inicio al login

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 /*
 app.get("/", (req, res) => {
